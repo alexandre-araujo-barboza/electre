7d7c0386a7b575e145c2818c3798f12bb82d56e3
Alexandre Araujo Barbosa <alexandre.araujo.barboza@gmail.com>
2023-10-27 15:48:31 -0300

    SQL dump

--- modelo_futebol.sql
+++ modelo_futebol.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 26/10/2023 às 19:59
--- Versão do servidor: 10.4.28-MariaDB
--- Versão do PHP: 8.2.4
+-- Tempo de geração: 27-Out-2023 às 20:47
+-- Versão do servidor: 10.4.24-MariaDB
+-- versão do PHP: 7.4.29
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `modelo_futebol`
+-- Estrutura da tabela `modelo_futebol`
 --
 
 CREATE TABLE `modelo_futebol` (
@@ -34,20 +34,21 @@ CREATE TABLE `modelo_futebol` (
   `cartoes` float(9,2) DEFAULT 0.00,
   `defesas` float(9,2) DEFAULT 0.00,
   `passes errados` float(9,2) DEFAULT 0.00
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Despejando dados para a tabela `modelo_futebol`
+-- Extraindo dados da tabela `modelo_futebol`
 --
 
 INSERT INTO `modelo_futebol` (`alternativa`, `gols`, `faltas`, `cartoes`, `defesas`, `passes errados`) VALUES
-('america', 20.00, -8.00, 0.00, 7.00, -15.00),
-('bangu', 8.00, -4.00, -2.00, 5.00, -36.00),
-('botafogo', 26.00, -10.00, 0.00, 8.00, -17.00),
-('flamengo', 18.00, 0.00, 0.00, 12.00, -4.00),
-('fluminense', 9.00, -12.00, -1.00, 5.00, -20.00),
-('vasco', 12.00, -8.00, -1.00, 9.00, -14.00),
-('peso', 5.00, 3.00, 4.00, 2.00, 1.00);
+('america', 20.00, 8.00, 0.00, 7.00, 15.00),
+('bangu', 8.00, 4.00, 2.00, 5.00, 36.00),
+('botafogo', 26.00, 10.00, 0.00, 8.00, 17.00),
+('flamengo', 18.00, 0.00, 0.00, 12.00, 4.00),
+('fluminense', 9.00, 12.00, 1.00, 5.00, 20.00),
+('vasco', 12.00, 8.00, 1.00, 9.00, 14.00),
+('peso', 5.00, 3.00, 4.00, 2.00, 1.00),
+('meta', 1.00, -1.00, -1.00, 1.00, -1.00);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
